07feb0270ccb7e42c6ff70dc708e0db901b74d95
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-31 21:46:15 -0300

    upload

--- index.js
+++ index.js
@@ -2,45 +2,49 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 
 const app = express();
+const email = 'danmazzeu9@gmail.com'
+const password = "fpgq mbja jnkv ejws"
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true, // use TLS
+  secure: true,
   auth: {
-    user: "danmazzeu9@gmail.com",
-    pass: "fpgq mbja jnkv ejws",
+    user: email,
+    pass: password,
   },
 });
 
-// Middleware para interpretar os dados da URL (query string)
-app.use(express.urlencoded({ extended: true })); // Habilita a interpretação de query string
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/enviar_email', (req, res) => { // Mudança para app.get
-  const { destinatario, assunto, mensagem } = req.query; // Dados da URL
+// Usage
+// https://codesnode-production.up.railway.app/sendmail?message=teste
+
+app.get('/sendmail', (req, res) => {
+  const { to, subject, message } = req.query;
 
   const mailOptions = {
-    from: 'danmazzeu9@gmail.com',
-    to: 'danmazzeu9@gmail.com', // Usa o destinatário da URL
-    subject: 'Assunto', // Usa o assunto da URL
-    text: mensagem // Usa a mensagem da URL
+    from: email,
+    to: email,
+    subject: 'Hacking',
+    text: message
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error sending email:', error);
       res.status(500).json({
-        mensagem: 'Erro ao enviar email',
-        erro: error.message, // Include the error message
-        errorDetails: error // Include the whole error object (for debugging)
+        message: 'Error sending email',
+        erro: error.message,
+        errorDetails: error
       });
     } else {
       console.log('Email sent:', info.response);
-      res.json({ mensagem: 'Email enviado com sucesso', info: info.response });
+      res.json({ message: 'Email sent successfully', info: info.response });
     }
   });
 });
 
 app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+  console.log('Server on port 3000');
 });
