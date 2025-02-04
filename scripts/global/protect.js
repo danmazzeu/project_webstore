fac7749225a4867c32b0a1e170d0a5d28442fa51
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-03 22:23:12 -0300

    upload

--- scripts/global/protect.js
+++ scripts/global/protect.js
@@ -1,6 +1,3 @@
 document.addEventListener('contextmenu', (event) => {
     event.preventDefault();
 });
-
-console.log("Elaborado e Desenvolvido por Daniel Mazzeu.");
-console.log("Todos os direitos reservados " + new Date().getFullYear() + ".");
