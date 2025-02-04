b5f489c371f88bb7ba28301bc6710b111d9e8c71
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-04 08:42:22 -0300

    upload

--- scripts/dev/main.js
+++ scripts/dev/main.js
@@ -6,11 +6,13 @@ products.forEach(product => {
     const card = document.createElement('div');
     card.classList.add('card');
 
+    const description = product.description.length > 117 ? product.description.substring(0, 117) + "..." : product.description;
+
     card.innerHTML = `
         <img src="${product.image}" alt="${product.name}">
         <h1>${product.name}</h1>
         <span>R$ ${product.value.toFixed(2)}</span>
-        <p>${product.description}</p>
+        <p>${description}</p>
         <a href="product.html?id=${product.id}" class="button">Comprar</a>
     `;
 
