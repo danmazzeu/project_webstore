22149f4a2411739a1a1d5363f5d8ced6a0e45999
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-08 13:06:27 -0300

    Upload

--- scripts/global/protect.js
+++ scripts/global/protect.js
@@ -5,7 +5,5 @@ document.addEventListener('contextmenu', (event) => {
 let attempts = localStorage.getItem('attempts') || 0;
 
 if (attempts && attempts >= 5) {
-    alert("You have exceeded the maximum number of attempts.  You are now being redirected.");
     window.location.href = 'blocked.html';
-    return;
 }
