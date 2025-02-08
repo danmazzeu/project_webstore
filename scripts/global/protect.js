467cac8f8ade316d3fa39fd27f688d4f0ac87050
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-08 13:00:55 -0300

    Upload

--- scripts/global/protect.js
+++ scripts/global/protect.js
@@ -1,3 +1,10 @@
 document.addEventListener('contextmenu', (event) => {
     event.preventDefault();
 });
+
+let attempts = localStorage.getItem('attempts');
+
+if (attempts >= 5) {
+    window.location.href = 'blocked.html';
+    return;
+}
